79170c4dcb7dc799397bd2cec2857e80cbdecf7f
Junielka <jjunielka4@gmail.com>
2016-09-01 17:30:35 -0600

    Practica con profesor Dionicio

---
+++ Practica 010916/calcularRectangulo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Calcular Rectangulo</title>
+</head>
+<body>
+<form method="post" action="Rectangulo.php">
+     Ingrese su primer valor: <input type="text" name="a"/> <br>
+	 Ingrese su segundo valor: <input type="text" name="b"/> <br>
+	 Ingrese su primer valor: <input type="text" name="c"/> <br>
+
+     <input type="radio" name="operador" value="area"> area<br>
+     <input type="radio" name="operador" value="expresion"> expresion <br>
+     
+    
+    
+     <input type="submit" name="enviar" value="Resultado">
+     
+
+</form>	
+</body>
+</html>
